95579a0c826a1697e7c21427696c2926605d8f90
jamesferry84 <jrferry@gmail.com>
2015-03-15 18:50:52 +0000

    added bootstrap and scripts

--- index.php
+++ index.php
@@ -10,8 +10,19 @@
 <html>
 <head>
     <title></title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    <p>Hello this is index.html</p>
+    <div class="container">
+        <div class="navbar navbar-default">
+            <a class="navbar-brand" href="#">Brand</a>
+            <ul class="nav navbar-nav">
+                <li><a href="#">Item 1</a></li>
+                <li><a href="#">Item 2</a></li>
+                <li><a href="#">Item 3</a></li>
+                <li><a href="#">Item 4</a></li>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
