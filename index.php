dcca1388ab86929dde39c9d7a9ba483a41404a09
jamesferry84 <jrferry@gmail.com>
2015-03-15 18:15:45 +0000

    This is the initial commit with index.php
     Please enter the commit message for your changes. Lines starting

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 15/03/2015
+ * Time: 18:12
+ */
+?>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <p>Hello this is index.html</p>
+</body>
+</html>
